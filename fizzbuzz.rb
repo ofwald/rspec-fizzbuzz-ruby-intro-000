2f85747fb0da1687ad77b9992895e4a3f0177599
Otto Wald <ofwald@gmail.com>
2019-07-23 19:36:17 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  if (int % 5 = 0 && int % 3 == 0)
+    return 'fizzbuzz'
+  elsif int % 5 = 0
+    return 'buzz'
+  elsif int % 3 = 0
+    return 'fizz'
+  else
+    return nil
+    
+    
+  
+end
